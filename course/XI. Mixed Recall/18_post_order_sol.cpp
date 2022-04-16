a5be9c446153938ae0c525dd3b7f2a22946100cb
ankitagrawal97 <ankit.k.agrawal97@gmail.com>
2022-04-16 12:50:35 +0530

    Added another problem and its solution

---
+++ course/XI. Mixed Recall/18_post_order_sol.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Node {
+  public:
+    std::string val;
+    Node* left;
+    Node* right;
+
+
+    Node(std::string initialVal) {
+      val = initialVal;
+      left = nullptr;
+      right = nullptr;
+    }
+};
+
+void postOrder(Node* root, vector<string> &vals){
+  if(!root){
+    return;
+  }
+  postOrder(root->left,vals);
+  postOrder(root->right,vals);
+  vals.push_back(root->val);
+}
+
+std::vector<std::string> postOrder(Node* root) {
+  vector<string> vals;
+  postOrder(root,vals);
+  return vals;
+}
+
+
+
+void run() {
+  // this function behaves as `main()` for the 'run' command
+  // you may sandbox in this function, but should not remove it
+}
