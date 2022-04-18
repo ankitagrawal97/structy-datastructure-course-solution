16332f3e1f8a03f3c424981bb287a8d541e0c53e
ankitagrawal97 <ankit.k.agrawal97@gmail.com>
2022-04-18 17:41:51 +0530

    added new question and its solution

---
+++ course/III. Linked List/5_reverse_list_sol.cpp
@@ -0,0 +1,32 @@
+#include <string>
+
+class Node {
+  public:
+    std::string val;
+    Node* next;
+
+    Node(std::string initialVal) {
+      val = initialVal;
+      next = nullptr;
+    }
+};
+
+
+
+Node* reverseList(Node* head) {
+  Node* prev = nullptr;
+  Node* curr = head;
+  while(curr){
+    Node* next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
+
+
+void run() {
+  // this function behaves as `main()` for the 'run' command
+  // you may sandbox in this function, but should not remove it
+}
