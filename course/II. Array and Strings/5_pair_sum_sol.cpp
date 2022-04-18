301c76f50f0e5011cea55c1dd014262c1429ad4b
ankitagrawal97 <ankit.k.agrawal97@gmail.com>
2022-04-18 16:21:04 +0530

    Added another problem and its solution

---
+++ course/II. Array and Strings/5_pair_sum_sol.cpp
@@ -0,0 +1,18 @@
+#include <array>
+#include <vector>
+#include <unordered_map>
+
+std::array<int, 2> pairSum(std::vector<int> numbers, int target) {
+  std::unordered_map<int, int> previousNums;
+  for (int i = 0; i < numbers.size(); i += 1) {
+    int complement = target - numbers[i];
+    if (previousNums.count(complement) > 0) {
+      return std::array<int, 2> { i, previousNums[complement] };
+    }
+    previousNums[numbers[i]] = i;
+  }
+}
+void run() {
+  // this function behaves as `main()` for the 'run' command
+  // you may sandbox in this function, but should not remove it
+}
